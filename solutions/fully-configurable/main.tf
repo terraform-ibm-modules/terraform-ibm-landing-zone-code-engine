3a5a6432d65628bf900f64e5e785fcdfbe70a543
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-12-13 23:20:46 +0000

    fix(deps): update dependencies (#23)

--- solutions/fully-configurable/main.tf
+++ solutions/fully-configurable/main.tf
@@ -9,7 +9,7 @@ locals {
 
 module "resource_group" {
   source                       = "terraform-ibm-modules/resource-group/ibm"
-  version                      = "1.4.3"
+  version                      = "1.4.6"
   existing_resource_group_name = var.existing_resource_group_name
 }
 
@@ -19,7 +19,7 @@ module "resource_group" {
 
 module "project" {
   source            = "terraform-ibm-modules/code-engine/ibm//modules/project"
-  version           = "4.7.5"
+  version           = "4.7.7"
   name              = local.code_engine_project_name
   resource_group_id = module.resource_group.resource_group_id
 }
