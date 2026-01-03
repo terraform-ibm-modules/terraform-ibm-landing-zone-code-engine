b3ff56b0f53bf9397a86cb8859e857f0463602d6
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-01-03 14:24:29 +0000

    fix(deps): update terraform terraform-ibm-modules/cloud-logs/ibm to v1.10.18

--- solutions/fleets-quickstart/main.tf
+++ solutions/fleets-quickstart/main.tf
@@ -347,7 +347,7 @@ module "cloud_logs" {
   count             = var.enable_cloud_logs ? 1 : 0
   depends_on        = [module.cos_buckets]
   source            = "terraform-ibm-modules/cloud-logs/ibm"
-  version           = "1.10.17"
+  version           = "1.10.18"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   instance_name     = local.icl_name
