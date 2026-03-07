0abc12efb754c50af22643ba989dd7f87190280a
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-03-07 22:32:24 +0000

    fix(deps): update dependencies

--- solutions/fleets-quickstart/main.tf
+++ solutions/fleets-quickstart/main.tf
@@ -205,7 +205,7 @@ locals {
 
 module "vpc" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "8.15.8"
+  version           = "8.15.9"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   name              = "vpc"
@@ -356,7 +356,7 @@ module "cloud_logs" {
   count             = var.enable_cloud_logs ? 1 : 0
   depends_on        = [module.cos_buckets]
   source            = "terraform-ibm-modules/cloud-logs/ibm"
-  version           = "1.12.6"
+  version           = "1.12.7"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   instance_name     = local.icl_name
